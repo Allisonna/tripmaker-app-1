0676aa368b9fa89d3f14fb1d12cae9809257f629
Allisonna <167255619+Allisonna@users.noreply.github.com>
2024-08-16 09:20:31 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "spots#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:show, :edit, :update]
 end
